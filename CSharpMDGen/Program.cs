c9c6b296d47d48e68fb2973901951f6228d7964b
Hibob5555556 <cayden.lunt@gmail.com>
2025-04-24 20:25:44 -0600

    started working on XML parsing

--- CSharpMDGen/Program.cs
+++ CSharpMDGen/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Xml.Linq;
 
 namespace CSharpMDGen
 {
@@ -11,113 +12,152 @@ namespace CSharpMDGen
             // set the current directory to the one containing the file
             Directory.SetCurrentDirectory("../../../");
 
-            string fileName = "sampleProg.cs";
+            var pathToConfig = "./MDGenConfig.xml";
+            var fileNamesMap = ConfigParser.LoadFileNames(pathToConfig);
 
-            // read in the csharp file
-            StreamReader reader = new($"./{fileName}");
-            string file = reader.ReadToEnd();
-            reader.Close();
+            foreach (var entry in fileNamesMap)
+                Console.WriteLine($"docId: {entry.Key}, fileName: {entry.Value}");
 
-            // get the tree of the file
-            string className;
-            List<string> methodNames = [];
-            List<string> constructorNames = [];
-            List<Tuple<string, List<string>, List<string>>> classToMethods = []; // class, method, constructor
-            SyntaxTree tree = CSharpSyntaxTree.ParseText(file);
+            //string fileName = "sampleProg.cs";
 
-            foreach (var classNode in tree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>())
+            //// read in the csharp file
+            //StreamReader reader = new($"./{fileName}");
+            //string file = reader.ReadToEnd();
+            //reader.Close();
+
+            //// get the tree of the file
+            //string className;
+            //List<string> methodNames = [];
+            //List<string> constructorNames = [];
+            //List<Tuple<string, List<string>, List<string>>> classToMethods = []; // class, method, constructor
+            //SyntaxTree tree = CSharpSyntaxTree.ParseText(file);
+
+            //foreach (var classNode in tree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>())
+            //{
+            //    className = classNode.Identifier.Text;
+
+            //    // Get all method declarations within this class
+            //    foreach (var method in classNode.Members.OfType<MethodDeclarationSyntax>())
+            //    {
+            //        methodNames.Add(method.Identifier.Text);
+            //    }
+
+            //    foreach (var constructor in classNode.Members.OfType<ConstructorDeclarationSyntax>())
+            //    {
+            //        constructorNames.Add(constructor.Identifier.Text);
+            //    }
+
+            //    classToMethods.Add(new Tuple<string, List<string>, List<string>>(className, [.. methodNames], [.. constructorNames]));
+            //    methodNames.Clear();
+            //    constructorNames.Clear();
+            //}
+
+            //// create a list of class names
+            //List<string> names = [];
+            //foreach (var tpl in classToMethods)
+            //    names.Add(tpl.Item1);
+
+            //// create a markdown file
+            //MDFile mdFile = MDGen.Create("CodeDocumentation", "Inventory System Documentation");
+
+            //// create an introduction section
+            //Section intro = MDGen.CreateSection("Introduction", $"- This file contains the documentation for the Inventory System file `{fileName}`.", "Arial");
+
+            //// create an ordered list of classes
+            //List<string> styleizedNames = [];
+            //foreach (var name in names)
+            //    styleizedNames.Add($"`{name}`");
+            //OL methods = MDGen.CreateOL([.. styleizedNames], "Classes:");
+            //MDGen.AddListElement(intro, methods, ordered: true);
+            //MDGen.AddSection(mdFile, intro);
+
+            //// create sections
+            //List<Section> sections = [];
+            //foreach (var name in names)
+            //    sections.Add(MDGen.CreateSection(name, $"- The following methods are contained within the `{name}` class:", "Arial"));
+
+            //// populate each section
+            //foreach (var section in sections.ToArray())
+            //{
+            //    List<string> classMethods = [];
+            //    List<string> classConstructors = [];
+            //    string cn = section.Title.Replace("## ", "").Trim();
+            //    foreach (var sec in classToMethods)
+            //    {
+            //        if (sec.Item1 == cn)
+            //        {
+            //            foreach (var method in sec.Item2)
+            //            {
+            //                if (sec.Item1 == cn)
+            //                    classMethods.Add($"`{method}`");
+            //            }
+            //            foreach (var constructor in sec.Item3)
+            //            {
+            //                if (sec.Item1 == cn)
+            //                    classConstructors.Add($"`{constructor}`");
+            //            }
+            //        }
+            //    }
+
+            //    // create lists from the methods and constructors
+            //    UL l = MDGen.CreateUL([.. classMethods.ToArray()], "Methods:", headingSize: 5);
+            //    UL l2 = MDGen.CreateUL([.. classConstructors.ToArray()], "Constructors:", headingSize: 5);
+
+            //    // add the lists to a section
+            //    if (classMethods.Count > 0)
+            //        MDGen.AddListElement(section, l, ordered: false, padBottom: true);
+
+            //    if (classConstructors.Count > 0)
+            //    {
+            //        Text text = MDGen.CreateText($"The following constructors are contained within the `{cn}` class:");
+            //        MDGen.AddText(section, text, bullet: true);
+            //        MDGen.AddListElement(
+            //            section, 
+            //            l2, 
+            //            ordered: false, 
+            //            smallTab: false, 
+            //            padTitle: true, 
+            //            padBottom: true
+            //        );
+            //    }
+            //    MDGen.AddSection(mdFile, section);
+            //}
+
+            //// write the file to disk
+            //MDGen.WriteToFile(mdFile);
+        }
+    }
+
+    public class ConfigParser
+    {
+        public static Dictionary<string, string> LoadFileNames(string configPath)
+        {
+            var fileNames = new Dictionary<string, string>();
+
+            try
             {
-                className = classNode.Identifier.Text;
+                XDocument doc = XDocument.Load(configPath);
 
-                // Get all method declarations within this class
-                foreach (var method in classNode.Members.OfType<MethodDeclarationSyntax>())
+                var fileNameElements = doc.Descendants("FileNames")
+                                          .Descendants("file");
+
+                foreach (var fileElem in fileNameElements)
                 {
-                    methodNames.Add(method.Identifier.Text);
-                }
+                    string docId = fileElem.Attribute("docId")?.Value;
+                    string fileName = fileElem.Value?.Trim();
 
-                foreach (var constructor in classNode.Members.OfType<ConstructorDeclarationSyntax>())
-                {
-                    constructorNames.Add(constructor.Identifier.Text);
-                }
-
-                classToMethods.Add(new Tuple<string, List<string>, List<string>>(className, [.. methodNames], [.. constructorNames]));
-                methodNames.Clear();
-                constructorNames.Clear();
-            }
-
-            // create a list of class names
-            List<string> names = [];
-            foreach (var tpl in classToMethods)
-                names.Add(tpl.Item1);
-
-            // create a markdown file
-            MDFile mdFile = MDGen.Create("CodeDocumentation", "Inventory System Documentation");
-
-            // create an introduction section
-            Section intro = MDGen.CreateSection("Introduction", $"- This file contains the documentation for the Inventory System file `{fileName}`.", "Arial");
-
-            // create an ordered list of classes
-            List<string> styleizedNames = [];
-            foreach (var name in names)
-                styleizedNames.Add($"`{name}`");
-            OL methods = MDGen.CreateOL([.. styleizedNames], "Classes:");
-            MDGen.AddListElement(intro, methods, ordered: true);
-            MDGen.AddSection(mdFile, intro);
-
-            // create sections
-            List<Section> sections = [];
-            foreach (var name in names)
-                sections.Add(MDGen.CreateSection(name, $"- The following methods are contained within the `{name}` class:", "Arial"));
-
-            // populate each section
-            foreach (var section in sections.ToArray())
-            {
-                List<string> classMethods = [];
-                List<string> classConstructors = [];
-                string cn = section.Title.Replace("## ", "").Trim();
-                foreach (var sec in classToMethods)
-                {
-                    if (sec.Item1 == cn)
+                    if (!string.IsNullOrWhiteSpace(docId) && !string.IsNullOrWhiteSpace(fileName))
                     {
-                        foreach (var method in sec.Item2)
-                        {
-                            if (sec.Item1 == cn)
-                                classMethods.Add($"`{method}`");
-                        }
-                        foreach (var constructor in sec.Item3)
-                        {
-                            if (sec.Item1 == cn)
-                                classConstructors.Add($"`{constructor}`");
-                        }
+                        fileNames[docId] = fileName;
                     }
                 }
-
-                // create lists from the methods and constructors
-                UL l = MDGen.CreateUL([.. classMethods.ToArray()], "Methods:", headingSize: 5);
-                UL l2 = MDGen.CreateUL([.. classConstructors.ToArray()], "Constructors:", headingSize: 5);
-
-                // add the lists to a section
-                if (classMethods.Count > 0)
-                    MDGen.AddListElement(section, l, ordered: false, padBottom: true);
-
-                if (classConstructors.Count > 0)
-                {
-                    Text text = MDGen.CreateText($"The following constructors are contained within the `{cn}` class:");
-                    MDGen.AddText(section, text, bullet: true);
-                    MDGen.AddListElement(
-                        section, 
-                        l2, 
-                        ordered: false, 
-                        smallTab: false, 
-                        padTitle: true, 
-                        padBottom: true
-                    );
-                }
-                MDGen.AddSection(mdFile, section);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading XML: {ex.Message}");
             }
 
-            // write the file to disk
-            MDGen.WriteToFile(mdFile);
+            return fileNames;
         }
     }
 }
---
+++ CSharpMDGen/SampleProg1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleProject
+{
+    /// <summary>
+    /// Represents a basic task with a name and a status.
+    /// </summary>
+    public class Task
+    {
+        /// <summary>
+        /// Gets or sets the name of the task.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the task is completed.
+        /// </summary>
+        public bool IsCompleted { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the Task class.
+        /// </summary>
+        /// <param name="name">The name of the task.</param>
+        public Task(string name)
+        {
+            Name = name;
+            IsCompleted = false;
+        }
+
+        /// <summary>
+        /// Marks the task as completed.
+        /// </summary>
+        public void Complete()
+        {
+            IsCompleted = true;
+        }
+    }
+
+    /// <summary>
+    /// Manages a list of tasks.
+    /// </summary>
+    public class TaskManager
+    {
+        private readonly List<Task> _tasks = new List<Task>();
+
+        /// <summary>
+        /// Adds a new task to the manager.
+        /// </summary>
+        /// <param name="taskName">The name of the task to add.</param>
+        public void AddTask(string taskName)
+        {
+            _tasks.Add(new Task(taskName));
+        }
+
+        /// <summary>
+        /// Completes the task with the given name.
+        /// </summary>
+        /// <param name="taskName">The name of the task to complete.</param>
+        public void CompleteTask(string taskName)
+        {
+            var task = _tasks.Find(t => t.Name == taskName);
+            if (task != null)
+            {
+                task.Complete();
+            }
+        }
+
+        /// <summary>
+        /// Displays all tasks and their completion status.
+        /// </summary>
+        public void DisplayTasks()
+        {
+            foreach (var task in _tasks)
+            {
+                Console.WriteLine($"- {task.Name} (Completed: {task.IsCompleted})");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Entry(string[] args)
+        {
+            TaskManager manager = new TaskManager();
+            manager.AddTask("Write parser");
+            manager.AddTask("Test parser");
+            manager.CompleteTask("Write parser");
+            manager.DisplayTasks();
+        }
+    }
+}
