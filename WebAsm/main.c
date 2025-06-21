fc474c349157210a6cb45b05c6727a12447c5184
Hibob5555556 <cayden.lunt@gmail.com>
2025-06-21 17:12:21 -0600

    added comments to WebAsm project

--- WebAsm/main.c
+++ WebAsm/main.c
@@ -3,24 +3,31 @@
 #include <stdlib.h>
 #include <emscripten.h>
 
+// add 2 numbers
 EMSCRIPTEN_KEEPALIVE
 int add(int a, int b) 
 {
     return a + b;
 }
 
+// multiply 2 numbers
 EMSCRIPTEN_KEEPALIVE
 int mult(int a, int b) 
 {
     return a * b;
 }
 
+// generate n numbers of the fibonacci sequence up to 46
 EMSCRIPTEN_KEEPALIVE
 int* fib(int count) 
 {
+    // create array
     int* fibNums = (int*)malloc(count * sizeof(int));
+
+    // ensure it is valid
     if (!fibNums) return NULL;
 
+    // iterate until we reach the desired number
     for (int i = 0; i < count; i++) {
         if (i == 0) {
             fibNums[i] = 0;  // or 1 if you prefer starting at 1
@@ -30,5 +37,7 @@ int* fib(int count)
             fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
         }
     }
+
+    // return our array
     return fibNums;
 }
